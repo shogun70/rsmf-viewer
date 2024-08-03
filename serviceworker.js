a58e6b505e74ecdc81c84190cd2dc29bb4c6334d
Sean Hogan <sean.hogan@nuix.com>
2024-08-03 15:32:47 +1000

    Fix for Firefox and refactor shared code out of serviceworker.js and index.html -> filestore.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,10 @@
+importScripts(
+    './js/filestore.js'
+);
+
+const BASE_DIR = 'rsmf';
+let fileStore;
+
 self.addEventListener('install', function(event) {
     // The promise that skipWaiting() returns can be safely ignored.
     self.skipWaiting();
@@ -7,30 +14,32 @@ self.addEventListener('install', function(event) {
     // of event.waitUntil();
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', async (event) => {
     // Claim any clients immediately, so that the page will be under SW control without reloading.
     event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
      let url = new URL(event.request.url);
-     console.info(url);
-     if (url.origin === self.location.origin && url.href.startsWith(self.registration.scope + 'rsmf/')) {
-         event.respondWith(lookup(url, event));
+     console.debug(url);
+     if (fileStore == null) fileStore = new FileStore([BASE_DIR]);
+     if (url.origin === self.location.origin && url.href.startsWith(self.registration.scope + fileStore.getBasePath())) {
+         event.respondWith(lookup(url, fileStore, event));
      }
 });
 
-function lookup(url, event) {
-    return navigator.storage.getDirectory()
-        .then(opfsRoot => {
-            let pathname = url.href.replace(self.registration.scope + 'rsmf/', ''); // FIXME configurable basePath
+function lookup(url, fileStore, event) {
+
+    return Promise.resolve()
+        .then(() => {
+            let pathname = url.href.replace(self.registration.scope + fileStore.getBasePath(), '');
             pathname = decodeURI(pathname);
-            console.info(pathname);
-            return opfsRoot.getFileHandle(pathname); // FIXME can throw
+            console.debug(pathname);
+            return fileStore.getFileHandle(pathname);
         })
         .then(fileHandle => fileHandle.getFile())
         .then(file => {
-            console.info(file);
+            console.debug(file);
             return new Response(file.stream());
         })
         .catch(reason => {
