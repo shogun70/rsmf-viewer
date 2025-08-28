48a86ed4cca04bbfe6717e5f2d84a60bc3535ef0
Sean Hogan <sean.hogan@nuix.com>
2025-08-28 18:33:50 +1000

    Migrating towards just table lookup for RSMF data

--- js/rsmfadapter.js
+++ js/rsmfadapter.js
@@ -9,59 +9,115 @@ class RsmfAdapter
     #eventsOrdered;
     #eventsById = new Map();
     #eventsByConversationId = new Map();
-    #rootEventsByConversationId = new Map();
+    #rootEvents = [];
     #eventsByParentId = new Map();
-    #uncategorizedEvents = [];
 
     constructor(manifest)
     {
+        this.#eventsByConversationId.set('', []);
         this.#manifest = manifest;
 
         this.#manifest['participants'].forEach(participant => {
             var id = participant['id'];
-            if (typeof id === 'string') this.#participantsById.set(id, participant);
-            else console.warn('RSMF participant has invalid ID');
+            if (typeof id === 'string') {
+                if (this.#eventsByParentId.has(id)) {
+                    console.warn('Duplicate RSMF participant ID', id);
+                }
+                this.#participantsById.set(id, participant);
+            }
+            else console.warn('RSMF participant has invalid ID', id);
         });
         this.#manifest['conversations'].forEach(conversation => {
             var id = conversation['id'];
-            if (typeof id === 'string') this.#conversationsById.set(id, conversation);
-            else console.warn('RSMF conversation has invalid ID');
+            if (typeof id === 'string') {
+                if (this.#conversationsById.has(id)) {
+                    console.warn('Duplicate RSMF conversation ID', id);
+                }
+                this.#conversationsById.set(id, conversation);
+            }
+            else console.warn('RSMF conversation has invalid ID', id);
         });
         this.#eventsOrdered = this.#manifest['events']
             .sort(RsmfAdapter.eventComparator);
 
         this.#eventsOrdered.forEach(event => {
             var id = event['id'];
-            if (typeof id === 'string') this.#eventsById.set(id, event);
-            else if (id != null) console.warn('RSMF event has invalid ID');
+            if (typeof id === 'string') {
+                if (this.#eventsById.has(id)) {
+                    console.warn('Duplicate RSMF event ID', id);
+                }
+                this.#eventsById.set(id, event);
+            }
+            else if (id != null) console.warn('RSMF event has invalid ID', id);
         });
-        this.#eventsOrdered.forEach(event => {
-            const id = event['id'];
-            let isUncategorized = true;
 
-            const conversationId = event['conversation'];
-            if (typeof conversationId === 'string') {
+        this.#eventsOrdered.forEach(event => {
+            const id = RsmfAdapter.getStringOrNull(event, 'id');
+            let conversationId = RsmfAdapter.getStringOrNull(event, 'conversation');
+            if (conversationId !== null) {
+                let conversation = this.#conversationsById.get(conversationId);
+                if (conversation == null) {
+                    console.warn("Found event with non-existent conversation ID", conversationId);
+                    conversation = {
+                        virtual: true,
+                        id: conversationId,
+                    }
+                    this.#conversationsById.set(conversationId, conversation);
+                }
                 let events = RsmfAdapter.#getOrSet(this.#eventsByConversationId, conversationId, []);
                 events.push(event);
-                isUncategorized = false;
             }
 
-            var parentId = event['parent'];
-            if (typeof parentId === 'string') {
+            let parentId = RsmfAdapter.getStringOrNull(event, 'parent');
+            if (parentId != null) {
+                let parent = this.#eventsById.get(parentId);
+                if (parent !== null) {
+                    // TODO more consistency checks
+                    let parentConversationId = RsmfAdapter.getStringOrNull(parent, 'conversation');
+                    if (parentConversationId !== null) {
+                        if (!conversationId) {
+                            console.warn("Inferring event conversation from parent");
+                            event['conversation'] = parentConversationId;
+                            conversationId = parentConversationId;
+                        }
+                        else if (conversationId !== parentConversationId) {
+                            console.warn("Ignoring parent relationship from different conversation", id, conversationId, parentId, parentConversationId);
+                            delete event['parent']; // FIXME is this appropriate??
+                            parentId = null;
+                            parent = null;
+                        }
+                    }
+                }
+                else {
+                    console.warn("Found event with non-existent parent ID", parentId);
+                    parent = {
+                        virtual: true,
+                        id: parentId,
+                        conversation: conversationId,
+                        timestamp: event.timestamp,
+                    }
+                    this.#eventsById.set(parentId, parent);
+                    this.#rootEvents.push(parent);
+                }
+            }
+
+            if (parentId !== null) {
                 let events = RsmfAdapter.#getOrSet(this.#eventsByParentId, parentId, []);
                 events.push(event);
-                isUncategorized = false;
             }
-            else if (typeof conversationId === 'string') {
-                let events = RsmfAdapter.#getOrSet(this.#rootEventsByConversationId, conversationId, []);
-                events.push(event);
-                isUncategorized = false;
+            else {
+                this.#rootEvents.push(event);
             }
 
-            if (isUncategorized) {
-                this.#uncategorizedEvents.push(event);
+            if (!conversationId) {
+                this.#eventsByConversationId.get('').push(event);
             }
         });
+    }
+
+    static getStringOrNull(object, field) {
+        let value = object[field];
+        return typeof value === 'string' ? value : null;
     }
 
     getParticipants()
@@ -76,7 +132,7 @@ class RsmfAdapter
 
     getConversations()
     {
-        return this.#manifest['conversations'];
+        return this.#conversationsById.values();
     }
 
     getConversationById(id)
@@ -103,14 +159,19 @@ class RsmfAdapter
 
     getRootEventsByConversationId(conversationId)
     {
-        return this.#rootEventsByConversationId.has(conversationId) ?
-            this.#rootEventsByConversationId.get(conversationId) :
-            conversationId === 'NONE' ? this.#uncategorizedEvents : [];
+        return this.getRootEvents(conversationId);
+    }
+
+    getRootEvents(...conversationIds)
+    {
+        var events = this.#rootEvents;
+        return conversationIds.length === 0 ? events :
+            events.filter(event => conversationIds.includes(event['conversation']));
     }
 
     getUncategorizedEvents()
     {
-        return this.#uncategorizedEvents;
+        return this.getEventsByConversationId('');
     }
 
     getEventsByParentId(parentId)
